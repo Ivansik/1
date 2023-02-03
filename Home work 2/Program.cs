2dd91c68008e910c7e0ab8b784a6f7c922958dc0
Иван Куприенко <ikuprienko670@gmail.com>
2023-02-03 20:54:40 +0300

    Сделал 2 работу

---
+++ Home work 2/Program.cs
@@ -0,0 +1,10 @@
+﻿//*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+
+int sum = int.Parse(Console.ReadLine()!);
+int sum2 = sum % 10;
+
+if (sum >= 100 && sum < 1000) Console.WriteLine($"Третья цифра введенного числа {sum2}");
+else if (sum < 100 ^ sum > 1000) Console.WriteLine("Третьей цифры нет");
