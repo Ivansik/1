143c969b326389a1f92dbc361133652ced322348
Иван Куприенко <ikuprienko670@gmail.com>
2023-02-03 20:43:27 +0300

    Сделал 1 задание

---
+++ Home work 1/Program.cs
@@ -0,0 +1,11 @@
+﻿//*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число");
+
+int sum = int.Parse(Console.ReadLine()!);
+int y = (sum / 10) % 10;
+
+if (sum >= 100 && sum < 1000) Console.WriteLine($"Средняя цифра введенного числа {y}");
+else if (sum < 100 ^ sum > 1000) Console.WriteLine($"Число {sum} не трехзначное");
