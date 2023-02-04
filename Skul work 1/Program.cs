26b70ebf1024b6eca68f9ded94d6bab73f43735f
Иван Куприенко <ikuprienko670@gmail.com>
2023-02-04 13:36:57 +0300

    Промежуточное задание 21

---
+++ Skul work 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите чило: ");
+int a = 
+int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (count <= a)
+{
+    Console.Write (count*count*count);
+    if (count != a) Console.Write(",");
+    count++;
+}
+
