a30a5894df7adb0039cc25024156d2d54a650be4
Иван Куприенко <ikuprienko670@gmail.com>
2023-02-16 19:31:41 +0300

    Сделал задание Суббота.

---
+++ Home work 10/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+double[] GetArray(int size, double minSum, double maxSum)
+{
+    double[] result = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = Math.Round(new Random().NextDouble() * (maxSum - minSum) + minSum, 2);
+    }
+    return result;
+}
+double ResultMax(double[] inArray)
+{
+    double max = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] > max)
+
+            max = inArray[i];
+    }
+    return max;
+}
+double ResultMin(double[] inArray)
+{
+    double min = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] < min)
+
+            min = inArray[i];
+    }
+    return min;
+}
+double[] array = GetArray(10, -10, 10);
+
+Console.WriteLine(String.Join(", ", array));
+
+double min = ResultMin(array);
+double max = ResultMax(array);
+
+Console.WriteLine($"Максимальное = {max}, Минимальное = {min}");
+Console.WriteLine($"Разниwа между минимальным и максимальным = {Math.Round(max-min, 2)}");
---
+++ Home work 6/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая принимает на вход число (N) и выдаёт сумму чисел от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите чило: ");
+int a = 
+int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (count <= a)
+{
+    
+}
