343b355439ee40b203dadf192400c6ac5582c4d6
Ivansik <iKuprienko670@gmail.com>
2023-06-15 20:55:33 +0300

    Заднине 34.36.38

---
+++ Payton Work/5.py
@@ -0,0 +1,18 @@
+#transformation = <???>
+#values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # или любой другой список
+#transormed_values = list(map(transformation, values))
+#print(transormed_values == values)
+##Напишите функцию same_by(characteristic, objects), которая
+проверяет, все ли объекты имеют одинаковое значение
+некоторой характеристики, и возвращают True, если это так.
+Если значение характеристики для разных объектов
+отличается - то False. Для пустого набора объектов, функция
+должна возвращать True. Аргумент characteristic - это
+функция, которая принимает объект и вычисляет его
+#характеристику.
+#Ввод: Вывод:
+#values = [0, 2, 10, 6] same
+#if same_by(lambda x: x % 2, values):
+#print(‘same’)
+#else:
+#print(‘different’)
